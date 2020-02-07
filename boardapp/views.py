cedf6ec7591f4d7bfa652c1eaa2eebc65523ec9a
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-02-07 16:40:52 +0900

    ログイン機能実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
+from .models import BoardModel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -25,10 +27,15 @@ def loginfunc(request):
 		user = authenticate(request, username=username2, password=password2)
 		if user is not None:
 			login(request, user)
-			return redirect('signup')
+			return redirect(request, 'list')
 		else:
 			return redirect('login')
 	return render(request, 'login.html')
+
+@login_required
+def listfunc(request):
+	object_list = BoardModel.objects.all()
+	return render(request, 'list.html', {'object_list':object_list})
 
 
 
