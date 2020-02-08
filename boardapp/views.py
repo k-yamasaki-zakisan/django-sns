6a85505a2d99d93d20e6f40f3a7da20b53c79604
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-02-08 14:13:26 +0900

    作成完了

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from .models import BoardModel
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -16,9 +18,8 @@ def signupfunc(request):
 			return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
 		except:
 			user = User.objects.create_user(username2, '', password2)
-		user = User.objects.create_user(username, '', password)
-		return render(request, 'signup.html', {'some': 100})
-	return render(request, 'signup.html', {'some': 100})
+			return render(request, 'signup.html', {'some': 100})
+	return render(request, 'list.html', {'some': 100})
 
 def loginfunc(request):
 	if request.method == 'POST':
@@ -27,7 +28,7 @@ def loginfunc(request):
 		user = authenticate(request, username=username2, password=password2)
 		if user is not None:
 			login(request, user)
-			return redirect(request, 'list')
+			return redirect('list')
 		else:
 			return redirect('login')
 	return render(request, 'login.html')
@@ -36,6 +37,37 @@ def loginfunc(request):
 def listfunc(request):
 	object_list = BoardModel.objects.all()
 	return render(request, 'list.html', {'object_list':object_list})
+
+def logoutfunc(request):
+	logout(request)
+	return redirect('login')
+
+def detailfunc(request, pk):
+	object = BoardModel.objects.get(pk=pk)
+	return render(request, 'detail.html', {'object':object})
+
+def goodfunc(request, pk):
+	post = BoardModel.ojbects.get(pk=pk)
+	post.good = post.good + 1
+	post.save()
+	return redirect('list')
+
+def readfunc(request, pk):
+	post = BoardModel.object.get(pk=pk)
+	post2 = request.user.get_username()
+	if post2 in post.readtext:
+		return  redirect('list')
+	else:
+		post.read += 1
+		post.readtext = post.readtext+' '+post2
+		post.save()
+		return redirect('list')
+
+class BoardCreate(CreateView):
+	template_name = 'create.html'
+	model = BoardModel
+	fields = ('title', 'content', 'author', 'image')
+	success_url = reverse_lazy('list')
 
 
 
